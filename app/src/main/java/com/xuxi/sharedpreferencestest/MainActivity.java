a6a0d7d3f2a354c6d935f73e77e2c724155f0ecf
zqzal <936952020@qq.com>
2019-08-05 11:38:07 +0800

    git status修改的状态，git checkout回退修改的内容，git reset取消添加，git diff查看所有文件的更改内容

--- app/src/main/java/com/xuxi/sharedpreferencestest/MainActivity.java
+++ app/src/main/java/com/xuxi/sharedpreferencestest/MainActivity.java
@@ -3,14 +3,16 @@ package com.xuxi.sharedpreferencestest;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "MainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        Log.d(TAG, "onCreate: " + "git status修改的状态，git checkout回退修改的内容，git reset取消添加，git diff查看所有文件的更改内容");
 
     }
 }
