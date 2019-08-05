4e73f7ee511f9528c7e6de92bd39c7a8f0e6ec53
zqzal <936952020@qq.com>
2019-08-05 11:58:26 +0800

    git branch分支的用法，git checkout 切换分支，git merge 合并分支，git branch -D 删除分支

--- app/src/main/java/com/xuxi/sharedpreferencestest/MainActivity.java
+++ app/src/main/java/com/xuxi/sharedpreferencestest/MainActivity.java
@@ -14,5 +14,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Log.d(TAG, "onCreate: " + "git status修改的状态，git checkout回退修改的内容，git reset取消添加，git diff查看所有文件的更改内容");
 
+        Log.d(TAG, "onCreate: " + "git branch分支的用法，git checkout 切换分支，git merge 合并分支，git branch -D 删除分支");
     }
 }
