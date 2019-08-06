9369fc4261c04d637f6dfa16004ca2d54a95a789
zqzal <936952020@qq.com>
2019-08-06 10:53:16 +0800

    SharedPreferences apply新增

--- app/src/main/java/com/xuxi/sharedpreferencestest/MainActivity.java
+++ app/src/main/java/com/xuxi/sharedpreferencestest/MainActivity.java
@@ -5,10 +5,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
+
+    private Button saveData;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,6 +21,19 @@ public class MainActivity extends AppCompatActivity {
         Log.d(TAG, "onCreate: " + "git status修改的状态，git checkout回退修改的内容，git reset取消添加，git diff查看所有文件的更改内容");
 
         Log.d(TAG, "onCreate: " + "git branch分支的用法，git checkout 切换分支，git merge 合并分支，git branch -D 删除分支");
+        saveData = findViewById(R.id.save_button);
+        saveData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                SharedPreferences.Editor editor = getSharedPreferences("data",MODE_PRIVATE).edit();
+                editor.putString("name","Andy");
+                editor.putInt("age",27);
+                editor.putBoolean("married",true);
+                editor.apply();
+
+            }
+        });
 
 
     }
