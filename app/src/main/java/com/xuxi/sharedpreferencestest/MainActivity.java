bb5e48049fd836b7a35861e766408e690c313e9e
zqzal <936952020@qq.com>
2019-08-06 11:04:04 +0800

    获取保存Share内

--- app/src/main/java/com/xuxi/sharedpreferencestest/MainActivity.java
+++ app/src/main/java/com/xuxi/sharedpreferencestest/MainActivity.java
@@ -14,6 +14,8 @@ public class MainActivity extends AppCompatActivity {
 
     private Button saveData;
 
+    private Button restoreData;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,6 +34,22 @@ public class MainActivity extends AppCompatActivity {
                 editor.putBoolean("married",true);
                 editor.apply();
 
+            }
+        });
+
+        restoreData = findViewById(R.id.restore_data);
+        restoreData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                SharedPreferences preferences = getSharedPreferences("data",MODE_PRIVATE);
+
+                String name = preferences.getString("name","");
+                int age = preferences.getInt("age",0);
+                boolean married = preferences.getBoolean("married",false);
+                Log.d(TAG, "name is " + name);
+                Log.d(TAG, "age is " + age);
+                Log.d(TAG, "married is " + married);
             }
         });
 
