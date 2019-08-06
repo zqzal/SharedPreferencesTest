4eaeee514f3d475b7b839ca5f93340498d13cefb
zqzal <936952020@qq.com>
2019-08-06 10:48:57 +0800

    xmlbutton提交

--- app/src/main/java/com/xuxi/sharedpreferencestest/MainActivity.java
+++ app/src/main/java/com/xuxi/sharedpreferencestest/MainActivity.java
@@ -2,6 +2,7 @@ package com.xuxi.sharedpreferencestest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 
@@ -15,5 +16,7 @@ public class MainActivity extends AppCompatActivity {
         Log.d(TAG, "onCreate: " + "git status修改的状态，git checkout回退修改的内容，git reset取消添加，git diff查看所有文件的更改内容");
 
         Log.d(TAG, "onCreate: " + "git branch分支的用法，git checkout 切换分支，git merge 合并分支，git branch -D 删除分支");
+
+
     }
 }
